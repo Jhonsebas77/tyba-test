22eb9b718ff54f69162508814ab79d34f5b5fbd3
Sebastian Otalora <jhonsebas77@outlook.com>
2021-06-23 17:16:17 -0500

    Add the home page and implement the Logout

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tyba_test/src/pages/login/login_page.dart';
+import 'package:tyba_test/src/providers/user_provider.dart';
+import 'package:tyba_test/src/utils/utils.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -8,14 +11,65 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final userProvider = new UserProvider();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Home',
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bienvenido'),
+        actions: [
+          _buildLogoutButton(context),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Text(
+                    'Bienvenido',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  _buildLogoutButton(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.logout),
+      onPressed: () => showAlertMessage(
+        context: context,
+        message: handlerAlertMessage('LOGOUT'),
+        title: handlerAlertTitle('LOGOUT'),
+        primaryAction: () => _logOut(context),
+        primaryActionText: 'Cerrar Sesion',
+      ),
+    );
+  }
+
+  _logOut(BuildContext context) {
+    userProvider.logOut();
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => LoginPage(),
+      ),
+      (route) => false,
     );
   }
 }
